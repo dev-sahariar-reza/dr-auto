020bb1248432355642ff9447c15129a76c08e7b5
dev-sahariar-reza <web.developer.sahariar.reza@gmail.com>
2023-10-24 13:49:12 +0600

    error pages added

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { Link, useRouteError } from "react-router-dom";
+import errorImage from "../../assets/error/error.jpg";
+import { FaArrowRightLong } from "react-icons/fa6";
 
 const ErrorPage = () => {
-  return <div></div>;
+  const { error } = useRouteError();
+
+  return (
+    <section>
+      <img src={errorImage} alt="error Picture" className="w-1/2 mx-auto" />
+
+      <div className="text-center">
+        <p className="text-3xl font-bold text-center my-8">{error?.message}</p>
+        <Link>
+          <button className="btn btn-neutral">
+            Back to home <FaArrowRightLong />
+          </button>
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default ErrorPage;
